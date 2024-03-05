f8200d5cd4d150042c98d9ab5da173c322ed7401
Lucky <Lucky.digital@gmail.com>
2024-03-05 16:44:08 +0530

    navbar design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,13 +17,17 @@ const Navbar = () => {
             <img src="https://saisaburihospitalbalakati.com/images/logo-grey.png" alt="logo" className='w-52'/>
             </Link>
           </div>
-          <ul className='flex items-center gap-12'>
+          <ul className='flex items-center gap-8'>
             {
                menuData.map((link,index)=>{
                return (
                     <>
-                      <li key={index} className='py-3 relative border-b-2 border-white hover:border-b-2  group'>
+                      <li key={index} className='py-3 relative border-b-2 border-white hover:border-b-2 flex items-center gap-1 group'>
                         <NavLink to={`/${link.link}`} >{link.name}</NavLink>
+                        {
+                       link.submenu && <span className='flex items-center mt-1 text-slate-600'><ion-icon name="caret-down-outline"></ion-icon></span>
+                      
+                        }
                         {
                           link.submenu && 
                           <ul className='absolute top-14 border hidden group-hover:block bg-white min-w-60 z-30'>
